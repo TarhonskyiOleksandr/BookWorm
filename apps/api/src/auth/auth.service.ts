79f16d8ca6cc5c9cbdbb10ec544563ea6f6de12d
OleksandrTarhonskyi <oleksandr@digisol.agency>
2025-04-11 08:28:40 +0300

    feat: added passportjs and local strategy

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { verify } from 'argon2';
+
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  async verifyUser(email: string, password: string) {
+    const user = await this.userService.findUser({ email }, { password: true });
+    if (!user) throw new UnauthorizedException('Invalid credentials');
+
+    const isPasswordMatch = await verify(user.password, password);
+
+    if (!isPasswordMatch)
+      throw new UnauthorizedException('Invalid credentials');
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
+  }
+}
